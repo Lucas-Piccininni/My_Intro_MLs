dfb9400537da784b60c6e111774ce4af7d774e83
Lucas-Piccininni <98120541+Lucas-Piccininni@users.noreply.github.com>
2023-07-06 15:34:13 +0900

    Updated files

--- TeachableMachine/script.js
+++ TeachableMachine/script.js
@@ -15,5 +15,13 @@
  * =============================================================================
  */
 
-const status = document.getElementById('status');
+const STATUS = document.getElementById('status');
+const VIDEO = document.getElementById('webcam');
+const ENABLE_CAM_BUTTON = document.getElementById('enableCam');
+const RESET_BUTTON = document.getElementById('reset');
+const TRAIN_BUTTON = document.getElementById('train');
+const MOBILE_NET_INPUT_WIDTH = 224;
+const MOBILE_NET_INPUT_HEIGHT = 224;
+const STOP_DATA_GATHER = -1;
+const CLASS_NAMES = [];
 status.innerText = 'Loaded TensorFlow.js - version: ' + tf.version.tfjs;
