e6ebffdd349b6d7c2a3221b1fc4315dbebbc2e64
Lucas-Piccininni <98120541+Lucas-Piccininni@users.noreply.github.com>
2023-07-06 16:29:46 +0900

    Update script.js
    Added more variables

--- TeachableMachine/script.js
+++ TeachableMachine/script.js
@@ -15,6 +15,7 @@
  * =============================================================================
  */
 
+//Const Variables
 const STATUS = document.getElementById('status');
 const VIDEO = document.getElementById('webcam');
 const ENABLE_CAM_BUTTON = document.getElementById('enableCam');
@@ -24,6 +25,16 @@ const MOBILE_NET_INPUT_WIDTH = 224;
 const MOBILE_NET_INPUT_HEIGHT = 224;
 const STOP_DATA_GATHER = -1;
 const CLASS_NAMES = [];
+
+//Redefinable Variables
+let mobilenet = undefined;
+let gatherDataState = STOP_DATA_GATHER;
+let videoPlaying = false;
+let trainingDataInputs = [];
+let trainingDataOutputs = [];
+let examplesCount = [];
+let predict = false;
+
 status.innerText = 'Loaded TensorFlow.js - version: ' + tf.version.tfjs;
 
 ENABLE_CAM_BUTTON.addEventListener('click', enableCam);
