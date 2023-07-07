544ec78a8bb5adf5037ca9109e142fdb8d98e3fc
Lucas-Piccininni <98120541+Lucas-Piccininni@users.noreply.github.com>
2023-07-07 11:16:54 +0900

    Updating the model head

--- TeachableMachine/script.js
+++ TeachableMachine/script.js
@@ -86,3 +86,25 @@ async function loadMobileNetFeatureModel() {
 
 // Call the function immediately to start loading.
 loadMobileNetFeatureModel();
+
+let model = tf.sequential();
+model.add(
+  tf.layers.dense({ inputShape: [1024], units: 128, activation: "relu" })
+);
+model.add(
+  tf.layers.dense({ units: CLASS_NAMES.length, activation: "softmax" })
+);
+
+model.summary();
+
+// Compile the model with the defined optimizer and specify a loss function to use.
+model.compile({
+  // Adam changes the learning rate over time which is useful.
+  optimizer: "adam",
+  // Use the correct loss function. If 2 classes of data, must use binaryCrossentropy.
+  // Else categoricalCrossentropy is used if more than 2 classes.
+  loss:
+    CLASS_NAMES.length === 2 ? "binaryCrossentropy" : "categoricalCrossentropy",
+  // As this is a classification problem you can record accuracy in the logs too!
+  metrics: ["accuracy"],
+});
